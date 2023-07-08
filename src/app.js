c6c04a919f5ef0564928829f11287a4582d095e2
pamelagcarvajal <130812633+pamelagcarvajal@users.noreply.github.com>
2023-07-08 00:03:31 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["yo", "usted", "el", "ella", "eso", "nosotros", "ellos"];
+let adj = ["suave", "grande", "dulce", "lindo", "veloz"];
+let noun = ["casa", "silla", "ciudad", "perro", "camisa"];
+let action = ["saltar", "comer", "jalar", "pintar", "escribir"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let n = 0; n < action.length; n++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + action[n] + ".com");
+      }
+    }
+  }
+}
